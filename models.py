e9ff12a8cb2e4370fec98e24cac909c70756b066
Gsr1989 <empresalorens@gmail.com>
2025-04-03 22:19:11 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class Vehiculo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    marca = db.Column(db.String(100))
-    linea = db.Column(db.String(100))
-    anio = db.Column(db.Integer)
-    numero_serie = db.Column(db.String(100), unique=True)
-    numero_motor = db.Column(db.String(100))
-    fecha = db.Column(db.DateTime)
+    marca = db.Column(db.String(100), nullable=False)
+    linea = db.Column(db.String(100), nullable=False)
+    anio = db.Column(db.Integer, nullable=False)
+    numero_serie = db.Column(db.String(100), unique=True, nullable=False)
+    numero_motor = db.Column(db.String(100), nullable=False)
+    fecha = db.Column(db.DateTime, default=datetime.utcnow)
